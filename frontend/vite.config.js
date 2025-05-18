33c6f3d34bd15d335f8c218e25901b3544b353f4
Konstantin Shagan <konstantinshagan@gmail.com>
2025-05-18 15:10:58 +0300

    added allowedHosts in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['frontend-project-12-rr1a.onrender.com'],
     port: 5002,
     proxy: {
       '/api': {
