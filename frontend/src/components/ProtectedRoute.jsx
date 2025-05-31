85ee51f8b6ad089b43ba9aa1478a0558466ce2fa
Konstantin Shagan <konstantinshagan@gmail.com>
2025-05-31 15:22:23 +0300

    protected route, step 3

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import { useLocation, Navigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const ProtectedRoute = ({children}) => {
     const location = useLocation();
+    const navigate = useNavigate();
+
     const token = localStorage.getItem('AuthorizationToken');
     
     if (!token) {
-        return <Navigate to="/login" state={{from: location}} replace/>
+        navigate('/login', {state: {from: location, replace: true}});
+        return null; //т.к. работа navigate() происходит асинхронно, то надо возвращать null, чтобы прервать текущий рендер.
     }
 
     return children;
 }
+
+//если есть токен, показывает дочерние компоненты.
